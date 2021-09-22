51a4b095cc33aa242dd3cedb3f73ee4a2a2aaf85
AvelyV <avely.voore@gmail.com>
2021-09-23 08:43:45 +1000

    added tty-prompt gem

--- budgety.rb
+++ budgety.rb
@@ -1,1 +1,10 @@
-puts "hello"
+require "tty-prompt"
+
+main_menu = TTY::Prompt.new
+
+main_menu.select("What would you like to do?", %w(New\ entry Monthly\ Overview Delete\ an\ entry))
+main_menu_input = gets
+
+if main_menu_input == "New entry"
+    puts "Yesssssss"
+end
