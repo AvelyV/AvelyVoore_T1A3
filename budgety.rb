d38ec38138fceae3e3e2c4bd3ce6d43433072c12
AvelyV <avely.voore@gmail.com>
2021-09-23 10:19:03 +1000

    main menu working with tty-prompt

--- budgety.rb
+++ budgety.rb
@@ -1,10 +1,18 @@
-require "tty-prompt"
 
-main_menu = TTY::Prompt.new
+# prints main menu
+def main_menu
+  require "tty-prompt"
+  main_menu = TTY::Prompt.new
 
-main_menu.select("What would you like to do?", %w(New\ entry Monthly\ Overview Delete\ an\ entry))
-main_menu_input = gets
+  input = main_menu.select("What would you like to do?", ["New Entry", "Budget Period Overview", "Delete an Entry"])
 
-if main_menu_input == "New entry"
-    puts "Yesssssss"
+  if input == "New Entry"
+    puts "This is new entry"
+  elsif input == "Budget Period Overview"
+    puts "This is overview"
+  elsif input == "Delete an Entry"
+    puts "This is where you delete and entry"
+  end
 end
+
+main_menu
---
+++ modules/new_entry.rb
@@ -0,0 +1,16 @@
+module NewEntry
+
+require 'tty-prompt'
+
+entry = prompt.collect do
+    key(:comment).ask("comment: ")
+  
+    key(:price).ask("Price: ", convert: :float)
+
+    key(:date).ask("Date: ", convert: :date, value: "DD/MM/YYYY")
+  
+    key(:category) do
+    # somehow make them choose from categories
+    # insert an array with all the categories
+    end
+  end
