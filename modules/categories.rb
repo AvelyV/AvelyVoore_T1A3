8e1f0b6ab9635f78ec5c422db627f0863c90b331
AvelyV <avely.voore@gmail.com>
2021-09-24 20:58:12 +1000

    Added add_cat method

--- modules/categories.rb
+++ modules/categories.rb
@@ -1,5 +1,6 @@
 require "tty-prompt"
 
+# remove items from catArray
 def remove_cat
   catArray = ['Home', 'Food', 'Transport', 'Bills', 'Entertainment', 'Splurge', 'Other']
   d = TTY::Prompt.new
@@ -9,4 +10,16 @@ def remove_cat
   pp catArray
 end
 
+# add items to catArray
+def add_cat
+  catArray = ['Home', 'Food', 'Transport', 'Bills', 'Entertainment', 'Splurge', 'Other']
+  v = TTY::Prompt.new
+  w = v.ask('Which category would you like to add?') do |q|
+    q.modify :strip, :capitalize
+  end
+  catArray << w
+  pp catArray
+end
+
 remove_cat
+add_cat
